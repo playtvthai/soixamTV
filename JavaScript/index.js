d1d24dae46582a032d6807ae62926e6bd04356d7
lmg159z <ghidz.tk@gmail.com>
2025-03-07 22:34:05 +0700

    update full add iframe

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,23 +1,5 @@
 var video = document.getElementById('myVideo');
-function stream(videoSrc) {
-  if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED,function(){
-       video.play();
-            });
-  } else if (
-    video.canPlayType('application/vnd.apple.mpegurl')
-   )
-   { 
-    video.src = videoSrc;
-    video.play();
-    video.autoplay = True
-    video.muted = False
-  }
-}  
-  
+
 ////////////////////////////
 
  function play(idStream, tag) {
@@ -29,9 +11,15 @@ function stream(videoSrc) {
     return response.json(); // Chuyển dữ liệu phản hồi thành JSON
   })
   .then(data => {
-    console.log(data[idStream].streamLink);
-    stream(data[idStream].streamLink)
-     })
+    if (data[idStream].style == "iframe") {
+      iframe(data[idStream].streamLink)
+    }
+    if (data[idStream].style == "m3u8") {
+      stream(data[idStream].streamLink)
+    }
+    
+    
+  })
   .catch(error => {
     console.error("Lỗi khi gọi API:", error.message);
   });  
@@ -39,4 +27,56 @@ function stream(videoSrc) {
  }
  
  
-var a = 17363636
+ 
+ 
+ 
+ ///////////
+ 
+ 
+ function iframe(linkStream){
+   const idHTML = document.getElementById("video")
+   idHTML.innerHTML = `
+    <iframe src="${linkStream}" 
+    class="ifvideo"
+    width="100%"
+    allow="autoplay"  
+    muted
+   allowfullscreen
+   ></iframe>
+   `
+ }
+ 
+ 
+ ///////
+ function stream(videoSrc) {
+   const idHTML = document.getElementById("video")
+   idHTML.innerHTML = `
+      
+      <video src="../../wordspage/video/cho.mp4"  class="video-section" id="myVideo"  poster="../../wordspage/image/poster/TV SHOW_20250120_172203_0000.png"   loop autoplay controls
+    </video>`;
+  
+  hls(videoSrc)
+}  
+  
+  
+  
+function hls(videoSrc) {
+ const  video = document.getElementById('myVideo');
+
+  if (Hls.isSupported()) {
+  var hls = new Hls();
+  hls.loadSource(videoSrc);
+  hls.attachMedia(video);
+  hls.on(Hls.Events.MANIFEST_PARSED, function() {
+    video.play();
+  });
+} else if (
+  video.canPlayType('application/vnd.apple.mpegurl')
+)
+{
+  video.src = videoSrc;
+  video.play();
+  video.autoplay = true
+  video.muted = true
+}
+}
